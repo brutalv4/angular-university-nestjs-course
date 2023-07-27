65d8e3f40d89df3cb441bc513b16bb518b541004
Vladimir Bredikhin <brutal.v4@gmail.com>
2023-07-27 15:42:02 +0300

    8. How to Debug a NestJs Backend using the Chrome Dev Tools

--- rest-api/src/courses/controllers/courses.controller.ts
+++ rest-api/src/courses/controllers/courses.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { findAllCourses } from '../../../db-data';
+import { Course } from './../../../../shared/course';
+
 @Controller()
 export class CoursesController {
   constructor() {}
 
-  @Get('/api/hello-world')
-  async helloWord() {
-    return 'Hello World!';
+  @Get('/api/courses')
+  async findAllCourses(): Promise<Course[]> {
+    return findAllCourses() as Course[];
   }
 }
