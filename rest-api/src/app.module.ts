973a40a4b373a428d62b16cdffbc3556b794a238
Vladimir Bredikhin <brutal.v4@gmail.com>
2023-12-11 15:11:27 +0200

    12. Setting Up Mongoose - Creating our first Schema

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CoursesModule } from './courses/courses.module';
 
+const { MONGO_DB_USERNAME, MONGO_DB_PASSWORD, MONGO_DB_HOST } = process.env;
+const MONGODB_CONNECTION = `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@${MONGO_DB_HOST}/nestjs-course?retryWrites=true&w=majority`;
+
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    CoursesModule,
+    MongooseModule.forRoot(MONGODB_CONNECTION, {
+      useNewUrlParser: true,
+    }),
+  ],
 })
 export class AppModule {}
