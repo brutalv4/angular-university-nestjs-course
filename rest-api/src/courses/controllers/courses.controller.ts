e5f833e38b6347d5672f0778b8dca02f5bdc7364
Vladimir Bredikhin <brutal.v4@gmail.com>
2023-12-20 14:24:44 +0200

    15. NestJs CRUD Update - Repository Implementation

--- rest-api/src/courses/controllers/courses.controller.ts
+++ rest-api/src/courses/controllers/courses.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
 
 import { CoursesRepository } from '../repositories/courses.repository';
 import { Course } from './../../../../shared/course';
 
-@Controller()
+@Controller('courses')
 export class CoursesController {
   constructor(private coursesDB: CoursesRepository) {}
 
-  @Get('/api/courses')
+  @Get()
   async findAllCourses(): Promise<Course[]> {
     return this.coursesDB.findAll();
+  }
+
+  @Put(':courseId')
+  async updateCourse(
+    @Param('courseId') courseId: string,
+    @Body() changes: Partial<Course>,
+  ): Promise<Course> {
+    return this.coursesDB.update(courseId, changes);
   }
 }
