f59c677f93873b73080ab3ea0599e074a6395574
Vladimir Bredikhin <brutal.v4@gmail.com>
2024-01-13 17:29:49 +0200

    35. NestJs Authentication - Validating the User Password

--- rest-api/src/auth/auth.controller.ts
+++ rest-api/src/auth/auth.controller.ts
@@ -1,9 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Controller('login')
 export class AuthController {
+  constructor(
+    @InjectModel('User')
+    private userDb: Model<{
+      email: string;
+      roles: string[];
+      passwordHash: string;
+    }>,
+  ) {}
+
   @Post()
-  login(@Body('email') email: string, @Body('password') password: string) {
-    console.log('login');
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const user = await this.userDb
+      .findOne({ email })
+      .orFail(new UnauthorizedException('User does not exist'));
+
+    console.log(user);
   }
 }
--- rest-api/src/auth/auth.module.ts
+++ rest-api/src/auth/auth.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuthController } from './auth.controller';
+import { UserSchema } from './user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
