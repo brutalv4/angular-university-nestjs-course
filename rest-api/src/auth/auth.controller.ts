e4b8f664fe4c50d44a7d80e77ccd386daa589adb
Vladimir Bredikhin <brutal.v4@gmail.com>
2024-01-13 19:25:05 +0200

    36. Authenticating a User with JSON Web Tokens

--- rest-api/src/auth/auth.controller.ts
+++ rest-api/src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as hashAndSalt from 'password-hash-and-salt';
+import * as jwt from 'jsonwebtoken';
 
 @Controller('login')
 export class AuthController {
@@ -16,12 +18,26 @@ export class AuthController {
   @Post()
   async login(
     @Body('email') email: string,
-    @Body('password') password: string,
+    @Body('password') plainTextPassword: string,
   ) {
     const user = await this.userDb
       .findOne({ email })
       .orFail(new UnauthorizedException('User does not exist'));
 
-    console.log(user);
+    return new Promise((resolve, reject) => {
+      hashAndSalt(plainTextPassword).verifyAgainst(
+        user.passwordHash,
+        (err, ok) => {
+          if (!ok) reject(new UnauthorizedException());
+
+          const authJwtToken = jwt.sign(
+            { email, roles: user.roles },
+            process.env.JWT_SECRET,
+          );
+
+          resolve({ authJwtToken });
+        },
+      );
+    });
   }
 }
